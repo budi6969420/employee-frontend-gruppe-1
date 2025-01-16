bce64c695f99f4d9913487547fa7c0864d9fc441
Abdul.i1 <abdul.i1@schule.bremen.com>
2025-01-16 11:53:11 +0100

    login page 4.0

--- src/app/views/login-view/login-view.component.ts
+++ src/app/views/login-view/login-view.component.ts
@@ -1,14 +1,16 @@
-import {Component, OnInit} from '@angular/core';
-import {TokenService} from "../../services/token.service";
-import {Observable} from "rxjs";
-import {Router} from "@angular/router";
-import {FormsModule} from "@angular/forms";
+import { Component, OnInit } from '@angular/core';
+import { TokenService } from "../../services/token.service";
+import { Router } from "@angular/router";
+import { FormsModule } from "@angular/forms";
+import {NgClass, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-login-view',
   standalone: true,
   imports: [
-    FormsModule
+    FormsModule,
+    NgClass,
+    NgIf
   ],
   templateUrl: './login-view.component.html',
   styleUrl: './login-view.component.css'
@@ -17,27 +19,38 @@ export class LoginViewComponent implements OnInit {
 
   protected username: string = 'user';
   protected password: string = 'test';
+  protected passwordVisible: boolean = false;
 
   protected tokenService: TokenService;
   private route: Router;
+
   constructor(tokenService: TokenService, route: Router) {
     this.tokenService = tokenService;
     this.route = route;
   }
 
   ngOnInit(): void {
-        if (this.tokenService.isTokenValid()) this.navigateToHomeView();
+    if (this.tokenService.isTokenValid()) {
+      this.navigateToHomeView();
     }
+  }
 
   login(): void {
     this.tokenService.login(this.username, this.password).subscribe({
       complete: () => {
         this.navigateToHomeView();
       },
+      error: (err) => {
+        console.error('Login failed', err);
+      }
     });
   }
 
   private navigateToHomeView() {
     this.route.navigate(['/']);
+  }
+
+  togglePasswordVisibility(): void {
+    this.passwordVisible = !this.passwordVisible;
   }
 }
